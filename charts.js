58d411d457476984d36b6c96d0cb54bb81fe4a40
joZecodes <zachary.r.dom@gmail.com>
2022-03-20 19:41:12 -0600

    deliverable 1 complete

--- charts.js
+++ charts.js
@@ -63,26 +63,35 @@ function buildCharts(sample) {
     var sampleNumber = sampleArray.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
     var firstSample = sampleNumber[0];
-    
+
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    var sampleIds = firstSample.otu_ids;
+    var sampleLabels = firstSample.otu_labels.slice(0, 10).reverse();
+    var sampleValues = firstSample.sample_values.slice(0, 10).reverse();
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
+    var yticks = sampleIds.map(sampleObj => "OTU " + sampleObj).slice(0,10).reverse();
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
+    var barData = [{
+      x: sampleValues,
+      y: yticks,
+      type: "bar", orientation: "h",
+      text: sampleLabels
+    }];
+
     // 9. Create the layout for the bar chart. 
     var barLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      //showticklabels: true
      
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot("bar", barData, barLayout);
+ 
   });
 }
